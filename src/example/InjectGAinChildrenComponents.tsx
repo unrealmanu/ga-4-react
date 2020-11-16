b33a59d64dbed05c1b77410ac226138f99d9d4cc
manueltrebbi <manuel.trebbi@innoteam.it>
2020-11-16 22:32:01 +0100

    update example

--- src/example/InjectGAinChildrenComponents.tsx
+++ src/example/InjectGAinChildrenComponents.tsx
@@ -1,18 +1,33 @@
 import React, { useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import { GA4ReactResolveInterface } from '../lib/gtagModels';
 import GA4R from './../components/GA4RComponents';
 
-export const ExampleChild: React.FC<{ ga4: any }> = ({ ga4 }) => {
+export const ExampleChild: React.FC<{
+  ga4?: GA4ReactResolveInterface;
+  children?: any;
+}> = ({ ga4, children }) => {
   useEffect(() => {
-    console.log(ga4);
+    console.log('ga4', ga4);
   }, []);
 
-  return <div></div>;
+  return <div>{children}</div>;
 };
 
 export const ExampleContainer: React.FC = () => {
   return (
     <div>
-      <GA4R code="GA-CODE"></GA4R>
+      <GA4R code="GA-CODE">
+        <ExampleChild>Valid element to inject GA4</ExampleChild>
+        <div>is not possibile to inject GA4 in html element</div>
+      </GA4R>
     </div>
   );
 };
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ExampleContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
