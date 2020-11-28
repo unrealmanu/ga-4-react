4cee4065d5ee70756798b3577c39164726e43bc3
manueltrebbi <manuel.trebbi@innoteam.it>
2020-11-29 00:21:42 +0100

    improved useGA4React

--- src/hooks/useGA4React.tsx
+++ src/hooks/useGA4React.tsx
@@ -1,27 +1,32 @@
-import { useState, useEffect } from 'react';
+import { useState, useLayoutEffect } from 'react';
 import GA4React from '..';
 import { GA4ReactResolveInterface } from '../lib/gtagModels';
 
 export const useGA4React = (
   gaCode?: string
 ): GA4ReactResolveInterface | void => {
-  const [ga4, setGA4] = useState(!gaCode ? GA4React.getGA4React() : undefined);
-  useEffect(() => {
-    if (!GA4React.isInitialized() && gaCode) {
-      const ga4react = new GA4React(gaCode);
-      ga4react.initialize().then(
-        (ga4: GA4ReactResolveInterface) => {
-          setGA4(ga4);
-        },
-        (err: Error) => {
-          console.error(err);
-        }
-      );
+  const [ga4, setGA4] = useState<any>();
+  useLayoutEffect(() => {
+    if (gaCode) {
+      switch (GA4React.isInitialized()) {
+        case false:
+          const ga4react = new GA4React(gaCode);
+          ga4react.initialize().then(
+            (ga4: GA4ReactResolveInterface) => {
+              setGA4(ga4);
+            },
+            (err: Error) => {
+              console.error(err);
+            }
+          );
+          break;
+        case true:
+          setGA4(GA4React.getGA4React());
+          break;
+      }
     } else {
       setGA4(GA4React.getGA4React());
     }
-  });
+  }, [gaCode]);
   return ga4;
 };
-
-export default useGA4React;
